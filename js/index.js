92d66da1a8ec85791851a76b4b6f84e2b769a96e
porschelo <leavetwin@gmail.com>
2021-03-17 12:30:34 +0800

    index 商品輪播完成

--- js/index.js
+++ js/index.js
@@ -5,17 +5,32 @@
       slidesToScroll: 1,
       autoplaySpeed: 2000,  
       dots: true,
+      autoplay: true,
       arrows: false,
-      autoplaySpeed: 2000, 
+      autoplaySpeed: 8000, 
     });
  });
 
- $('.product-list').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: false,
-  // autoplaySpeed: 5000,
-});
+//  $('.product-list').slick({
+//   slidesToShow: '4',
+//   slidesToScroll: 1,
+//   autoplay: false,
+//   // autoplaySpeed: 5000,
+// });
+
+
+// <!-- Initialize Swiper -->
+ 
+    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      autoplay: true,
+      freeMode: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
 
 
 $(function() {
